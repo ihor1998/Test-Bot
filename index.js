1a4a44b0833007a097961a20d063c5ff5ccc15dc
ihor1998 <ihor.milshyn@gmail.com>
2021-05-18 12:50:02 +0300

    bot

---
+++ index.js
@@ -0,0 +1,19 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '1877929690:AAEGcU2Ytrn2sdBr66FTPS3NMshEaQh6fK8';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const name = msg.chat.first_name;
+
+  const animal = ['краб', 'слон', 'жираф'];
+  const random = Math.floor(Math.random() * 3);
+
+  if (name === 'Eugene') {
+    return bot.sendMessage(chatId, `${name}, краб`);
+  }
+
+  bot.sendMessage(chatId, `${name}, ${animal[random]}`);
+});
